19c54a6d3e3f142b20fcf90061210fa852347d37
yuyanggong <421736079@qq.com>
2017-01-09 17:34:43 +0800

    test

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express'),
+	fs = require('fs'),
 	app = new express(),
-	port = process.env.PORT || 3000,
-	mongo = require('mongodb').MongoClient;
+	port = process.env.PORT || 4000,
+	mongo = require('mongodb').MongoClient,
+	user = process.env.USER,
+	password = process.env.PASSWORD,
+	data_url = process.env.MONGO_URL;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -13,67 +17,67 @@ app.get('/', function(req, res) {
 	});
 });
 
-// app.get('/new/:url', function(req, res) {
-// 	var url = req.params.url;
-// 	console.log(url);
-// 	if(/^https?:\/\/([a-z]+\.)?[a-z]+\.[a-z]+$/i.test(url)) {
-// 		mongo.connect(data_url, function(err, db) {
-// 			let match_url, num;
-// 			if(err) return console.log(err);
-// 			else {
-// 				var collection = db.collection('url');
-// 				collection.find().toArray(function(err, urls) {
-// 					if(err) return console.error(err);
-// 					num = urls.length;
-// 					match_url = urls.filter(v => v["original_url"] === url);
-// 					if(match_url.length) {
-// 						res.json({"original_url":url,"short_url":match_url[0]["short_url"]});
-// 						db.close();
-// 					}
-// 					else {
-// 						collection.insert({ "original_url": url, "short_url":"https://little-url-yuyang.herokuapp.com/" + num, "order":num}, function(err, data) {
-// 							if(err) return console.log(err);
-// 							else {
-// 								res.json({ "original_url": url, "short_url":"https://little-url-yuyang.herokuapp.com/" + num });
-// 								db.close();
-// 							}
-// 						});
-// 					}
-// 				});
-// 			}
-// 		});
-// 	}
-// 	else {
-// 		res.json({"error":"Wrong url format, make sure you have a valid protocol and real site."});
-// 	}
-// })
+app.get('/new/:url', function(req, res) {
+	var url = req.params.url;
+	console.log(url);
+	if(/^https?:\/\/([a-z]+\.)?[a-z]+\.[a-z]+$/i.test(url)) {
+		mongo.connect(data_url, function(err, db) {
+			let match_url, num;
+			if(err) return console.log(err);
+			else {
+				var collection = db.collection('url');
+				collection.find().toArray(function(err, urls) {
+					if(err) return console.error(err);
+					num = urls.length;
+					match_url = urls.filter(v => v["original_url"] === url);
+					if(match_url.length) {
+						res.json({"original_url":url,"short_url":match_url[0]["short_url"]});
+						db.close();
+					}
+					else {
+						collection.insert({ "original_url": url, "short_url":"https://little-url-yuyang.herokuapp.com/" + num, "order":num}, function(err, data) {
+							if(err) return console.log(err);
+							else {
+								res.json({ "original_url": url, "short_url":"https://little-url-yuyang.herokuapp.com/" + num });
+								db.close();
+							}
+						});
+					}
+				});
+			}
+		});
+	}
+	else {
+		res.json({"error":"Wrong url format, make sure you have a valid protocol and real site."});
+	}
+})
 
-// app.get('/:num', function(req, res) {
-// 	var num = req.params.num;
-// 	if(/^\d+$/.test(num)) {
-// 		mongo.connect(data_url, function(err, db) {
-// 			if(err) return console.log(err);
-// 			else {
-// 				let collection = db.collection('url');
-// 				collection.find().toArray(function(err, urls) {
-// 					if(err) return console.log(err);
-// 					let match_url = urls.filter(v => v.order == num);
-// 					if(match_url.length) {
-// 						console.log(match_url[0]["original_url"])
-// 						res.redirect(match_url[0]["original_url"]);
-// 					}
-// 					else {
-// 						res.json({"error":"no match url"})
-// 					}
-// 					db.close();
-// 				});
-// 			}
-// 		});
-// 	}
-// 	else {
-// 		res.json({"error":"Wrong format, make sure you have a valid order or site."});
-// 	}
-// })
+app.get('/:num', function(req, res) {
+	var num = req.params.num;
+	if(/^\d+$/.test(num)) {
+		mongo.connect(data_url, function(err, db) {
+			if(err) return console.log(err);
+			else {
+				let collection = db.collection('url');
+				collection.find().toArray(function(err, urls) {
+					if(err) return console.log(err);
+					let match_url = urls.filter(v => v.order == num);
+					if(match_url.length) {
+						console.log(match_url[0]["original_url"])
+						res.redirect(match_url[0]["original_url"]);
+					}
+					else {
+						res.json({"error":"no match url"})
+					}
+					db.close();
+				});
+			}
+		});
+	}
+	else {
+		res.json({"error":"Wrong format, make sure you have a valid order or site."});
+	}
+})
 
 app.get('*', function(req, res) {
 	res.send('404 not found!');
